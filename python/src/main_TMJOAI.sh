2b197e17396407a97d53f37fc76bb66dc1cedeb2
Celia Le <luciacev-admin@0587394714.wireless.umich.net>
2021-04-07 10:32:45 -0400

    minor corrections

--- python/src/main_TMJOAI.sh
+++ python/src/main_TMJOAI.sh
@@ -13,26 +13,23 @@ IFS=',' read -a var < ${inputfile}
 
 if [[ " ${var[@]} " =~ " y " ]]
     then
-        Command="bash /app/python/src/main_training.sh -i ${inputfile} -o /app/python/out -s /app/src --seed1 1000 --seed_end 1010" #training
+        Command="bash /app/OAI/python/src/main_training.sh -i /app/OAI/python/$(basename ${inputfile}) -o /app/OAI/python/output -s /app/OAI/python/src --seed1 1000 --seed_end 1010" #training
     else 
-        Command="python3 /app/python/src/main_prediction.py ${inputfile} --folder /app/python/Models -o /app/python/out/Prediction.csv" #prediction
+        Command="python3 /app/OAI/python/src/main_prediction.py /app/OAI/python/$(basename ${inputfile}) --folder /app/OAI/python/Models -o /app/OAI/python/output/Prediction.csv" #prediction
 fi
 
 docker run --rm \
-    -v /Users/luciacev-admin/Documents/TMJOAI/OAI_github/python/Data.csv:/app/Data.csv \
-    -v /Users/luciacev-admin/Documents/TMJOAI/OAI_github/python/Models:/app/Models \
-    -v /Users/luciacev-admin/Documents/TMJOAI/OAI_github/python/${inputfile}:/app/$(basename ${inputfile}) \
-    -v /Users/luciacev-admin/Documents/TMJOAI/OAI_github/python/out:/app/out \
+    -v /Users/luciacev-admin/Documents/TMJOAI/OAI_github/python/Data.csv:/app/OAI/python/Data.csv \
+    -v /Users/luciacev-admin/Documents/TMJOAI/OAI_github/python/Models:/app/OAI/python/Models \
+    -v /Users/luciacev-admin/Documents/TMJOAI/OAI_github/${inputfile}:/app/OAI/python/$(basename ${inputfile}) \
+    -v /Users/luciacev-admin/Documents/TMJOAI/OAI_github/python/out:/app/OAI/python/output \
     tmjoai:latest \
     ${Command}
-    # -v /Users/luciacev-admin/Documents/TMJOAI/OAI_github/python/src:/app/src \
-
 
 # docker run --rm \
-#     -v /shiny-tooth/data/dcbia-filebrowser/source/tmjoai/Data.csv:/app/Data.csv \
-#     -v /shiny-tooth/data/dcbia-filebrowser/source/tmjoai/Models:/app/Models \
-#     -v /shiny-tooth/data/dcbia-filebrowser/source/tmjoai/src:/app/src \
-#     -v /shiny-tooth/data/dcbia-filebrowser/${inputfile}:/app/$(basename ${inputfile}) \
-#     -v /shiny-tooth/data/dcbia-filebrowser/$(dirname ${inputfile}):/app/out \
+#     -v /shiny-tooth/data/dcbia-filebrowser/source/tmjoai/Data.csv:/app/OAI/python/Data.csv \
+#     -v /shiny-tooth/data/dcbia-filebrowser/source/tmjoai/Models:/app/OAI/python/Models \
+#     -v /shiny-tooth/data/dcbia-filebrowser/${inputfile}:/app/OAI/python/$(basename ${inputfile}) \
+#     -v /shiny-tooth/data/dcbia-filebrowser/$(dirname ${inputfile}):/app/OAI/python/output \
 #     tmjoai:latest \
 #     ${Command}
