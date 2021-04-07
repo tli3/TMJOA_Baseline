ae3bac577ca98746917f18cc1043b4bfe412cd6d
Celia Le <luciacev-admin@0587394714.wireless.umich.net>
2021-04-07 11:16:46 -0400

    DSCI version

--- python/src/main_TMJOAI.sh
+++ python/src/main_TMJOAI.sh
@@ -18,18 +18,18 @@ if [[ " ${var[@]} " =~ " y " ]]
         Command="python3 /app/OAI/python/src/main_prediction.py /app/OAI/python/$(basename ${inputfile}) --folder /app/OAI/python/Models -o /app/OAI/python/output/Prediction.csv" #prediction
 fi
 
-docker run --rm \
-    -v /Users/luciacev-admin/Documents/TMJOAI/OAI_github/python/Data.csv:/app/OAI/python/Data.csv \
-    -v /Users/luciacev-admin/Documents/TMJOAI/OAI_github/python/Models:/app/OAI/python/Models \
-    -v /Users/luciacev-admin/Documents/TMJOAI/OAI_github/${inputfile}:/app/OAI/python/$(basename ${inputfile}) \
-    -v /Users/luciacev-admin/Documents/TMJOAI/OAI_github/python/out:/app/OAI/python/output \
-    tmjoai:latest \
-    ${Command}
-
 # docker run --rm \
-#     -v /shiny-tooth/data/dcbia-filebrowser/source/tmjoai/Data.csv:/app/OAI/python/Data.csv \
-#     -v /shiny-tooth/data/dcbia-filebrowser/source/tmjoai/Models:/app/OAI/python/Models \
-#     -v /shiny-tooth/data/dcbia-filebrowser/${inputfile}:/app/OAI/python/$(basename ${inputfile}) \
-#     -v /shiny-tooth/data/dcbia-filebrowser/$(dirname ${inputfile}):/app/OAI/python/output \
+#     -v /Users/luciacev-admin/Documents/TMJOAI/OAI_github/python/Data.csv:/app/OAI/python/Data.csv \
+#     -v /Users/luciacev-admin/Documents/TMJOAI/OAI_github/python/Models:/app/OAI/python/Models \
+#     -v /Users/luciacev-admin/Documents/TMJOAI/OAI_github/${inputfile}:/app/OAI/python/$(basename ${inputfile}) \
+#     -v /Users/luciacev-admin/Documents/TMJOAI/OAI_github/python/out:/app/OAI/python/output \
 #     tmjoai:latest \
 #     ${Command}
+
+docker run --rm \
+    -v /shiny-tooth/data/dcbia-filebrowser/source/tmjoai/Data.csv:/app/OAI/python/Data.csv \
+    -v /shiny-tooth/data/dcbia-filebrowser/source/tmjoai/Models:/app/OAI/python/Models \
+    -v /shiny-tooth/data/dcbia-filebrowser/${inputfile}:/app/OAI/python/$(basename ${inputfile}) \
+    -v /shiny-tooth/data/dcbia-filebrowser/$(dirname ${inputfile}):/app/OAI/python/output \
+    tmjoai:latest \
+    ${Command}
